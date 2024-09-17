37449e2a30b70267339a3ee27e73594073f9a14e
Derek <78566663+dvorakman@users.noreply.github.com>
2024-09-17 20:13:35 +1000

    Update app.py
    removed duplicate function

--- app.py
+++ app.py
@@ -74,23 +74,6 @@ def pad_single_hand_landmarks(landmark_list, handedness):
     else:
         raise ValueError("Handedness must be 'Left' or 'Right' or 'Both'")
 
-def calc_bounding_rect(image, landmarks):
-    image_width, image_height = image.shape[1], image.shape[0]
-
-    landmark_array = np.empty((0, 2), int)
-
-    for _, landmark in enumerate(landmarks.landmark):
-        landmark_x = min(int(landmark.x * image_width), image_width - 1)
-        landmark_y = min(int(landmark.y * image_height), image_height - 1)
-
-        landmark_point = [np.array((landmark_x, landmark_y))]
-
-        landmark_array = np.append(landmark_array, landmark_point, axis=0)
-
-    x, y, w, h = cv.boundingRect(landmark_array)
-
-    return [x, y, x + w, y + h]
-
 def calc_landmark_list(image, landmarks):
     image_width, image_height = image.shape[1], image.shape[0]
 
